9386ff17f465fd7e14969826505da05862b63c92
Carlos Eduardo <105376328+delfo2@users.noreply.github.com>
2022-12-12 16:18:42 -0300

    colocando novas funções (de forma assincrona) para acontecerem apos a pagina carregar

--- js/main.js
+++ js/main.js
@@ -7,23 +7,6 @@ async function getBuscarLivros () {
     let resposta = await fetch(endPoint);
     livros = await resposta.json();
     console.log(livros);
-    acrescentarLivros(livros);
-}
-
-function acrescentarLivros (listalivros) {
-    listalivros.forEach(livro => {
-        secaoLivros.innerHTML += `
-            <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">
-                ${livro.titulo}
-            </h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$${livro.preco}</p>
-            <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-            </div>
-            </div>
-        `
-    })
+    let livrosComDesconto = aplicarDesconto(livros);
+    acrescentarLivros(livrosComDesconto);
 }
